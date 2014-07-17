d69b7f28d87a45e1e64598628cc6c0a00aededad
Kyle Spraggs <spiffyjr@users.noreply.github.com>
2014-07-17 00:42:07 -0500

    Update IndexAction.php

--- package/application/src/Home/IndexAction.php
+++ package/application/src/Home/IndexAction.php
@@ -9,7 +9,7 @@ use Spiffy\Framework\Action\AbstractAction;
  * complexity of your controller layer and helps enforce FAT models SKINNY controllers. 
  * 
  * Controllers are called from Spiffy\Dispatch using the __invoke() method. Any parameters available to the
- * route are automatically injected into the __invoke() method. If you add methods to __invoke() that don't
+ * route are automatically injected into the __invoke() method. If you add arguments to __invoke() that don't
  * exist in the route you will get an exception.
  * 
  * For optional parameters simply set the default value as null, e.g., public function __invoke($id, $optional => null) 
